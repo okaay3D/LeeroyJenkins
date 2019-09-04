25718d8cd38a955a4227973ac687a282ffe46a6a
Marc H <marc@okaay.net>
2019-09-04 20:10:45 +0200

    fehler :(

--- src/net/okaay/leeroy/Main.java
+++ src/net/okaay/leeroy/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Hello World, Yannis riecht gut oder so");
         System.out.println("Aber richtig");
+
+        yannis hat nen kleinen
     }
 }
