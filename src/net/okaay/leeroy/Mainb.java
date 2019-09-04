f17c02e1c1d28f0e22f14126e88630fb6afd8ce5
Marc H <marc@okaay.net>
2019-09-04 19:45:08 +0200

    swag

--- src/net/okaay/leeroy/Mainb.java
+++ src/net/okaay/leeroy/Mainb.java
@@ -3,6 +3,6 @@ package net.okaay.leeroy;
 public class Mainb {
 
     public static void main(String... args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, Yannis stinkt");
     }
 }
